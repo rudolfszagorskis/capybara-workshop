3c5553e4b13baadf809fa9baf4b1d0a6b54b2895
Ulvis Goldbergs <ulvis.goldbergs@testdevlab.com>
2018-04-19 18:19:15 +0300

    Update after lecture 1.

--- features/step_definitions/signup_steps.rb
+++ features/step_definitions/signup_steps.rb
@@ -1,4 +1,8 @@
 When(/^I am on Appimation home page/) do
   visit('/')
   find(:css, '#start_button')
+  find(:xpath, "//button[@id = 'start_button']")
+  find(:css, "#video")
+  find(:css, ".feature-image")
+  find(:xpath, "//h3[contains(text(), 'Team up')]/preceding-sibling::img")
 end
